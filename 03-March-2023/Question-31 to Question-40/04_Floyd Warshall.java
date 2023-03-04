9d64a7e7bb4a4ab8b7aea841ee0cef4d52a2bb21
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-03-04 11:36:47 +0530

    Create 04_Floyd Warshall.java

---
+++ 03-March-2023/Question-31 to Question-40/04_Floyd Warshall.java
@@ -0,0 +1,42 @@
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        // Code here 
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=(int)1e9;
+                }
+                if(i==j) matrix[i][j]=0;
+            }
+        }
+        
+        for(int k=0;k<n;k++){
+            for(int j=0;j<n;j++){
+                for(int i=0;i<n;i++){
+                    matrix[i][j]=Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+                }
+            }
+        }
+        
+        // for negative cyscle you can check by this
+        
+        for(int i=0;i<n;i++){
+            if(matrix[i][i]<0){
+                System.out.println("negative cycle");
+            }
+        }
+        //////////////////////////////////
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==(int)1e9){
+                    matrix[i][j]=-1;
+                }
+            }
+        }
+    }
+}
